8dab5855aae98686a42b35758902d40115a0b9c8
Blaze <yunjingliu07@gmail.com>
2026-03-25 14:15:16 +0800

    Adding the complete StunEffect class

--- src/effects/StunEffect.java
+++ src/effects/StunEffect.java
@@ -1,12 +1,43 @@
 package effects;
 
+import domain.Combatant;
+
 public class StunEffect implements StatusEffects{
     private int turns = 2;
 
+    @Override
     public String getName(){
         return "Stunned!";
     }
 
+    @Override
+    public boolean blockAction(){
+        return false;
+    }
 
+    @Override
+    public int modifyAtk(){
+        return 0; // Defend adds atk by 0
+    }
+
+    @Override
+    public int modifyDef(){
+        return 0; // Defend adds def by 10
+    }
+
+    @Override
+    public int modifySpd(){
+        return 0; // Defend adds spd by 0
+    }
+
+    @Override
+    public void onTick(BattleContext ctx, Combatant target){
+        turns--;
+    }
+
+    @Override
+    public boolean isExpired(){
+        return turns <= 0;
+    }
 
 }

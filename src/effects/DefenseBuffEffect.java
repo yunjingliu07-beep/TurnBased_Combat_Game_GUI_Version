bf24d275bf579743dc9cf7e6f5f3cf8573b69893
Blaze <yunjingliu07@gmail.com>
2026-03-25 11:53:07 +0800

    Add the concrete class DefenseBuffEffect

---
+++ src/effects/DefenseBuffEffect.java
@@ -0,0 +1,42 @@
+package effects;
+
+import domain.Combatant;
+
+public class DefenseBuffEffect implements StatusEffects {
+    private int turns = 2; // Defense Buff lasts for 2 turns
+
+    @Override
+    public String getName() {
+        return "Defense Buff";
+    }
+
+    @Override
+    public boolean blockAction(){
+        return false; // Defend does not block enemy actions
+    }
+
+    @Override
+    public int modifyAtk(){
+        return 0; // Defend adds atk by 0
+    }
+
+    @Override
+    public int modifyDef(){
+        return 10; // Defend adds def by 10
+    }
+
+    @Override
+    public int modifySpd(){
+        return 0; // Defend adds spd by 0
+    }
+
+    @Override
+    public void onTick(BattleContext ctx, Combatant target){
+        turns--;
+    }
+
+    @Override
+    public boolean isExpired(){
+        return turns <= 0;
+    }
+}

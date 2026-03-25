37da7c0f6f773e41bf700e6c3fe4a066bda304c2
Blaze <yunjingliu07@gmail.com>
2026-03-25 11:21:50 +0800

    Adding the concrete class Inventory to be used in Player

---
+++ src/items/Inventory.java
@@ -0,0 +1,25 @@
+package items;
+
+import domain.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory{
+    private List<Items> items = new ArrayList<Items>();
+
+    public void addItem(Items i){
+        items.add(i);
+    }
+
+    public void useItem(int index, BattleContext ctx, Player p){
+        if(index >= 0 && index < items.size()){
+            Items itemUsed =  items.remove(index);
+            itemUsed.apply(ctx, p);
+        }
+        else{
+            System.err.println("Invalid item index");
+        }
+    }
+
+}

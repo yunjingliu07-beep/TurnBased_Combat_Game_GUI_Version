408eea29d8ac6bacbbc3131d78e21c40a281f589
Blaze <yunjingliu07@gmail.com>
2026-03-27 18:16:17 +0800

    Add new enemy types Wolf & Goblin

---
+++ src/domain/Goblin.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Goblin extends Enemy{
+
+    public Goblin() {
+        super("Goblin", 55, 35, 15, 25);
+    }
+
+}
---
+++ src/domain/Wolf.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class Wolf extends Enemy{
+
+    public Wolf(){
+        super("Wolf", 40, 45, 5, 35);
+    }
+
+}

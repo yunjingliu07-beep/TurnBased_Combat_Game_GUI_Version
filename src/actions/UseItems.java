5953a81b94e90de3cf80780f60514e5e6dccc879
Blaze <yunjingliu07@gmail.com>
2026-03-27 17:28:05 +0800

    Add new class UseItems

---
+++ src/actions/UseItems.java
@@ -0,0 +1,31 @@
+package actions;
+
+import domain.Combatant;
+import domain.Player;
+import items.Inventory;
+import ui.GameUI;
+
+public class UseItems implements Actions {
+
+    GameUI ui;
+
+    public UseItems(GameUI ui){
+        this.ui = ui;
+    }
+
+    @Override
+    public String actionName(){
+        return "UseItems";
+    }
+
+    @Override
+    public void execute(BattleContext ctx, Combatant p){
+        Player player = (Player) p;
+
+        int choice = ui.chooseItems();
+        Inventory playerInventory = player.getInventory();
+        playerInventory.useItem(choice, ctx, player);
+
+    }
+
+}

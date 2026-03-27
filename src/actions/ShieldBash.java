e420fe02142342fc5e6ce7296b5cf2268ded08a9
Blaze <yunjingliu07@gmail.com>
2026-03-27 17:48:48 +0800

    Add new skill ShieldBash

---
+++ src/actions/ShieldBash.java
@@ -0,0 +1,47 @@
+package actions;
+
+import domain.Combatant;
+import domain.Player;
+import effects.StunEffect;
+
+public class ShieldBash extends SpecialSkill{
+    private Combatant target;
+    private boolean consumeCooldown;
+
+    public ShieldBash(Combatant target, boolean consumeCooldown){
+        this.target = target;
+        this.consumeCooldown = consumeCooldown;
+    }
+
+    @Override
+    public String actionName(){
+        return "Shield Bash!";
+    }
+
+    @Override
+    public void execute(BattleContext ctx, Combatant actor){
+        Player player = (Player) actor;
+
+        // player's special skill is still on CD & not using the skill via power stone
+        if (consumeCooldown && !player.canUseSpecialSkill()) {
+            System.out.println(player.getName() + "'s Shield Bash is on cooldown for "
+                    + player.getSpecialCooldown() + " more turn(s).");
+        }
+
+        int dmg = Math.max(0, player.getAtk() - target.getDef());
+        System.out.println(actor.getName() + " uses Shield Bash on " + target.getName() + "!");
+        target.takeDamage(dmg);
+        if (!target.isAlive()){
+            System.out.println(actor.getName() + " has defeated "+ target.getName());
+        }
+        else{
+            target.addStatusEffect(new StunEffect());
+            System.out.println(target.getName() + " has been stunned for this turn and next turn");
+            System.out.println("The current status of the target is:\n" + target.getCurrentAttribute());
+        }
+
+        player.useSpecialSkill(consumeCooldown);
+
+    }
+
+}
